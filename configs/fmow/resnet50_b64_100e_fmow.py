cc1bb9923e8c04f9965992749cc09bfd3851668d
tanmlh <fahongzhang@gmail.com>
2022-10-06 15:21:26 +0000

    trivial

--- configs/fmow/resnet50_b64_100e_fmow.py
+++ configs/fmow/resnet50_b64_100e_fmow.py
@@ -6,24 +6,24 @@ evaluation = dict(
     interval=10, metric='accuracy',
     save_best='auto')  # save the checkpoint with highest accuracy
 
-expr_name = 'resnet50_b64_100e_fmow'
-init_kwargs = dict(
-    project='rsi_cls',
-    entity='tum-tanmlh',
-    name=expr_name,
-    resume='never'
-)
-log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook', by_epoch=False),
-        dict(type='TensorboardLoggerHook'),
-        dict(type='MMClsWandbHook',
-             init_kwargs=init_kwargs,
-             interval=201,
-             num_eval_images=20),
-        # dict(type='PseudoLabelingHook',
-        #      log_dir='work_dirs/pseudo_labels/deeplabv3plus_r50-d8_512x512_80k_loveda_r2u',
-        #      interval=1),
-    ])
+# expr_name = 'resnet50_b64_100e_fmow'
+# init_kwargs = dict(
+#     project='rsi_cls',
+#     entity='tum-tanmlh',
+#     name=expr_name,
+#     resume='never'
+# )
+# log_config = dict(
+#     interval=50,
+#     hooks=[
+#         dict(type='TextLoggerHook', by_epoch=False),
+#         dict(type='TensorboardLoggerHook'),
+#         dict(type='MMClsWandbHook',
+#              init_kwargs=init_kwargs,
+#              interval=201,
+#              num_eval_images=20),
+#         # dict(type='PseudoLabelingHook',
+#         #      log_dir='work_dirs/pseudo_labels/deeplabv3plus_r50-d8_512x512_80k_loveda_r2u',
+#         #      interval=1),
+#     ])
 
